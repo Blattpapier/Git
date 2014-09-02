47d8afaaa04106b10e8b9ada0bc284007763b565
david.mueller <davidmueller@imac23.local>
2014-09-02 09:43:34 +0200

    kdlfjs

--- src/gitbest.java
+++ src/gitbest.java
@@ -2,12 +2,10 @@
 public class gitbest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hallo");
+		
 
 	}
 
 }
 
-
-
-das isch s beste game ever !!!
